ab6a3e684bf5cca0847ab58150f2817b6f4762cf
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-25 23:39:24 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+
+<?php include "inc/header.php"?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 offset-md-3">
+
+					<h1>Hello World</h1>
+					
+				</div>
+			</div>
+		</div>
+
+<?php include "inc/footer.php"?>
+
+
+
+
