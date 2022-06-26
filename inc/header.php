7b82491d7a183dd50d458e7e4efca8d57b5487ff
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-27 04:01:21 +0600

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,8 @@
+<?php include "inc/db.php";?>
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +21,7 @@
 
 
 		<header>
-			<div class="container">
+			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-12">
 
@@ -24,7 +29,7 @@
 
 						<nav class="navbar navbar-expand-lg bg-light">
 						  	<div class="container-fluid">
-							    <a class="navbar-brand" href="#">Navbar</a>
+							    <a class="navbar-brand" href="index.php">Student Portal</a>
 							    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							      <span class="navbar-toggler-icon"></span>
 							    </button>
