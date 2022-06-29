546770abc6c4ef8f576421ee7baf2f28dc1afc84
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-30 04:18:01 +0600

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -2,9 +2,30 @@
 
 
 
+		
+		<!-- javaScript -->
+		<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
 		<!-- Bootstrap JavaScript Bundle with Popper -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+		<!-- DATA Table CDN -->
+		<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+
+		<!-- DATA Table CDN -->
+		<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+
+		<script type="text/javascript">
+			
+			$(document).ready(function () {
+
+		   		 $('#dataTable').DataTable();
+
+			});
+
+		</script>
+
+		
 
 	</body>
 </html>
