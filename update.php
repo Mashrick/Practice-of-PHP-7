06ac0fc429f67e8ff4adc234e0a5855b49954e67
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-30 04:15:37 +0600

    Add files via upload

---
+++ update.php
@@ -0,0 +1,28 @@
+
+<?php include"inc/header.php";?>
+
+
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 offset-3">
+
+					<h1>We are Updateing this part</h1>
+
+					<?php 
+
+						if (isset($_GET['id'])) {
+
+							echo $_GET['id'];
+
+						}
+
+
+
+					?>
+					
+				</div>
+			</div>
+		</div>
+
+<?php include"inc/footer.php";?>
