62744f012bcc01c75cb9e8650632a9b666598e63
Md. Mashrickul Haque <mashu.haque@gmail.com>
2022-06-30 04:16:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,75 +2,95 @@
 
 
 <?php include "inc/header.php";?>
+		
 
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<table class="table table-dark table-striped table-hover">
-						<thead>
+		<div class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<table class="table table-bordered table-dark table-striped table-hover" id="dataTable">
+							<thead>
 
-							<tr>
-							  <th scope="col">Serial Number</th>
-							  <th scope="col">Name</th>
-							  <th scope="col">Phone</th>
-							  <th scope="col">Email</th>
-							  <th scope="col">Address</th>
-							  <th scope="col">Join Date</th>
-							</tr>
+								<tr>
+								  <th scope="col">Serial Number</th>
+								  <th scope="col">Name</th>
+								  <th scope="col">Phone</th>
+								  <th scope="col">Email</th>
+								  <th scope="col">Address</th>
+								  <th scope="col">Join Date</th>
+								  <th scope="col">Action</th>
+								</tr>
 
-						</thead>
+							</thead>
 
-					  <tbody>
+						  <tbody>
 
-					  	<?php 
+						  	<?php 
 
-					  		$sql		=	"SELECT * FROM studentdata";
-					  		$allData	=	mysqli_query($db, $sql);
-					  		$numofData	=	mysqli_num_rows($allData);
+						  		$sql			=	"SELECT * FROM studentdata ORDER BY fullName ASC";
+						  		$allData		=	mysqli_query($db, $sql);
+						  		$numofData		=	mysqli_num_rows($allData);
+						  		$serialNumber	=	0;
 
-					  		if ($numofData <= 0) {
+						  		if ($numofData <= 0) {
 
-					  			echo '<div class="alert alert-info"><h3>There is no data</h3></div>';
+						  			echo '<div class="alert alert-info"><h3>There is no data</h3></div>';
 
-					  		} else {
+						  		} else {
 
-					  			while ($row = mysqli_fetch_assoc($allData) ) {
+						  			while ($row = mysqli_fetch_assoc($allData) ) {
 
-					  				$id 			=	$row['id'];
-					  				$fullName 		=	$row['fullName'];
-					  				$phone 			=	$row['phone'];
-					  				$emailAddress 	=	$row['emailAddress'];
-					  				$address 		=	$row['address'];
-					  				$joinDate 		=	$row['joinDate'];
-					  				
+						  				$id 			=	$row['id'];
+						  				$fullName 		=	$row['fullName'];
+						  				$phone 			=	$row['phone'];
+						  				$emailAddress 	=	$row['emailAddress'];
+						  				$address 		=	$row['address'];
+						  				$joinDate 		=	$row['joinDate'];
+						  				$serialNumber++;
+						  				
 
-					  	?>
+						  	?>
 
-					  		<tr>
-						      <th><?php 	echo $id 			?></th>						    
-						      <td><?php 	echo $fullName 		?></td>
-						      <td><?php 	echo $phone 		?></td>
-						      <td><?php 	echo $emailAddress	?></td>
-						      <td><?php 	echo $address 		?></td>
-						      <td><?php 	echo $joinDate 		?></td>
-					   		</tr>
+						  		<tr>
 
-					  	<?php				
+							      <td>	<?php 	echo $serialNumber	?>	</td>						    
+							      <td>	<?php 	echo $fullName 		?>	</td>
+							      <td>	<?php 	echo $phone 		?>	</td>
+							      <td>	<?php 	echo $emailAddress	?>	</td>
+							      <td>	<?php 	echo $address 		?>	</td>
+							      <td>	<?php 	echo $joinDate 		?>	</td>
 
-					  										
-					  			}
+							      <td>	
 
-					  		}
+							      	<div class="btn-group">
 
-					  	?>					  	
+							      		<a href="update.php?id=<?php echo $id; ?>" class="btn btn-sm btn-outline-success">Update</a>
+							      		<a href="" class="btn btn-sm btn-outline-danger">Delete</a>
+
+							      	</div>
+
+							      	
+
+							      </td>
+						   		</tr>
+
+						  	<?php				
+
+						  										
+						  			}
+
+						  		}
+
+						  	?>					  	
 
 
-					    
-					  </tbody>
-					</table>
+						    
+						  </tbody>
+						</table>
 
-					
-					
+						
+						
+					</div>
 				</div>
 			</div>
 		</div>
